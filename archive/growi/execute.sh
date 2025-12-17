b530dcb45dc751b1aabb060d7aa198c4612cc48c
Lukas <lukas.schulte@uni-passau.de>
2025-12-17 16:05:29 +0100

    moves growi to archive - old versions without tests

---
+++ archive/growi/execute.sh
@@ -0,0 +1,142 @@
+#!/bin/bash
+
+# -------------------------------------------------------------
+# ATTENTION! ONLY MODIFY THE ORIGINAL
+# THIS WILL BE COPIED TO EACH PROJECT'S FOLDER AUTOMATICALLY
+# -------------------------------------------------------------
+
+cd /app/repo
+starttime=$(date +%s)
+
+IS_NPM_MAIN_PM=$([[ "$package_manager" == npm* ]] && echo "true" || echo "false")
+export IS_NPM_MAIN_PM
+
+IS_YARN_MAIN_PM=$([[ "$package_manager" == yarn* ]] && echo "true" || echo "false")
+export IS_YARN_MAIN_PM
+
+IS_PNPM_MAIN_PM=$([[ "$package_manager" == pnpm* ]] && echo "true" || echo "false")
+export IS_PNPM_MAIN_PM
+
+IS_YARN_LEGACY=$(yarn --version | grep -q "^1\." && echo "true" || echo "false")
+
+echo "=== Starting run-coverage.sh ==="
+echo "Revision: $revision"
+echo "Commit date: $(date -d @$timestamp)"
+echo "Timestamp: $timestamp"
+echo "Package Manager: $package_manager"
+echo ""
+echo "=== System Information ==="
+uname -a
+echo ""
+echo "=== Linux Distribution ==="
+cat /etc/os-release
+echo ""
+echo "=== Git Version ==="
+git --version
+echo ""
+echo "=== CPU Information ==="
+nproc
+echo ""
+echo "=== Memory Information ==="
+free -h
+echo ""
+echo "=== Disk Information ==="
+df -h
+echo ""
+set -e
+echo "=== Current Folder ==="
+pwd
+echo ""
+echo "=== Git Checkout ==="
+git checkout "$revision"
+echo ""
+echo "=== Node Version ==="
+node --version
+echo ""
+echo "=== NPM Version ==="
+npm --version
+echo "npm main PM: $IS_NPM_MAIN_PM"
+echo ""
+echo "=== Yarn Version ==="
+yarn --version
+echo "Yarn main PM: $IS_YARN_MAIN_PM"
+echo "Legacy Yarn: $IS_YARN_LEGACY"
+echo ""
+
+echo "=== Setting up Package Managers ==="
+
+
+# Always set npm registry to Waypack
+# if [ "$IS_NPM_MAIN_PM" = "true" ] || [ "$package_manager" == "" ]; then
+#     echo " --> Setup npm"
+# fi
+echo " --> Setup npm"
+npm config set registry "http://waypack:3000/npm/$timestamp/"
+
+# Set up yarn if it's the main package manager or no package manager is specified
+if [ "$IS_YARN_MAIN_PM" = "true" ] || [ "$package_manager" == "" ]; then
+    echo " --> Setup yarn"
+    if [ "$IS_YARN_LEGACY" = "true" ]; then
+        echo " --> Setting yarn legacy registry to Waypack..."
+        yarn config set registry "http://waypack:3000/yarn/$timestamp/"
+    else
+        echo " --> Setting yarn modern registry to Waypack..."
+        yarn config set unsafeHttpWhitelist "waypack"
+        yarn config set npmRegistryServer "http://waypack:3000/yarn/$timestamp/"
+    fi
+fi
+
+# Set up pnpm only if it's the main package manager
+if [ "$IS_PNPM_MAIN_PM" = "true" ]; then
+    echo " --> Setup pnpm"
+    IS_PM_SPECIFIED=$(grep -q '"packageManager"' package.json && echo "true" || echo "false")
+    if [[ "$IS_PM_SPECIFIED" = "true" && -x "$(command -v corepack)" && "${package_manager}" == pnpm* ]]; then
+        echo " --> Setting up ${package_manager} via corepack..."
+        corepack enable
+        corepack prepare "${package_manager}" --activate
+    else 
+        echo " --> Installing pnpm globally via npm..."
+        npm install --no-fund -g pnpm
+    fi
+    pnpm config set registry "http://waypack:3000/npm/$timestamp/"
+
+    echo "=== PNPM Version ==="
+    pnpm --version
+    echo "pnpm main PM: $IS_PNPM_MAIN_PM"
+    echo ""
+fi
+echo ""
+
+
+echo "=== Calling install-and-run.sh ==="
+
+timeout 5400s bash ../install-and-run.sh
+
+
+PATTERNS=('coverage/**' 'packages/**/coverage/**' 'packages/**/**/coverage/**' "apps/**/coverage/**")
+IGNORE_PATTERNS=('*/node_modules/*')
+
+# Build the find command's ignore arguments
+ignore_args=()
+for ignore in "${IGNORE_PATTERNS[@]}"; do
+    ignore_args+=(-not -path "$ignore")
+done
+
+lcov_count=0
+for pattern in "${PATTERNS[@]}"; do
+    lcov_count=$((lcov_count + $(find . -path "./$pattern" -name "lcov.info" "${ignore_args[@]}" | wc -l)))
+done
+
+if [ "$lcov_count" -eq 0 ]; then
+    echo "Error: No lcov.info files found in any of the specified paths"
+    exit 1
+fi
+
+zip_file="../coverage/$(date -d @$timestamp '+%Y-%m-%d')-$revision.zip"
+zip -r "$zip_file" . -i "${PATTERNS[@]}" -x "${IGNORE_PATTERNS[@]}"
+
+echo "=== Coverage run completed ==="
+endtime=$(date +%s)
+elapsed=$((endtime - starttime))
+echo "Total time: $elapsed seconds"
+
---
+++ archive/growi/install-and-run.sh
@@ -0,0 +1,55 @@
+
+#!/bin/bash
+cd /app/repo
+
+if [ "$IS_PNPM_MAIN_PM" = "true" ]; then
+    echo "=== Running with pnpm ==="
+
+    pnpm install
+
+    set +e
+    pnpm dlx turbo run test --continue -- --coverage.enabled true
+    set -e
+elif [ "$IS_YARN_MAIN_PM" = "true" ]; then
+    echo "=== Running with yarn ==="
+
+    # Clean yarn.lock to avoid resolved URLs that point to the non-cached registry
+    sed -i '/^  resolved/d' yarn.lock
+    yarn install
+
+    # check if turbo.json exists
+    if [ -f "turbo.json" ]; then
+        echo " --> mono-repo detected via turbo.json"
+        yarn global add turbo
+        set +e
+        yarn exec turbo run test --continue --coverage
+        set -e
+    elif [ -f "lerna.json" ]; then
+        echo " --> mono-repo detected via lerna.json"
+        yarn global add lerna
+        yarn exec lerna init
+        set +e
+        # Notes:
+        # seems to fail with unknown option "coverage" in some (or all?) versions
+        # coverate is still generated though
+        yarn exec lerna run test --no-bail
+        set -e
+    else
+        echo " --> single-package repo detected"
+        set +e
+        yarn test --coverage --continue
+        set -e
+    fi
+else
+    echo "=== Running with npm ==="
+
+    # npm ci
+
+    # set +e
+    # npm run test -- --coverage --continue
+    # set -e
+
+    # npm not setup, throw error
+    echo "Error: npm is not set up in this project."
+    exit 1
+fi
