2753d05f3b61ef213c2c32b1bf9ea047656f4763
Lukas <lukas.schulte@uni-passau.de>
2026-01-27 16:37:42 +0100

    reverses to using existing scripts

--- projects/spreed/install-and-run.sh
+++ projects/spreed/install-and-run.sh
@@ -2,6 +2,7 @@
 cd /app/repo
 
 npm ci --no-fund
+npm install -g nyc --no-fund
 
 # Choosing to use nyc in both cases (for consistency with "aws-toolkit-vscode").
 # In principle, c8 would work in "spreed".
@@ -11,21 +12,13 @@ npm ci --no-fund
 # npx c8 --r=lcov -o ../$COVERAGE_REPORT_PATH npm run test
 
 set +e
-if grep -q "test:unit" package.json; then
-    echo "Running tests with test:unit script..."
-
-    npx nyc \
-        --temp-directory="$COVERAGE_REPORT_PATH/nyc" \
-        --reporter=lcov \
-        --report-dir="$COVERAGE_REPORT_PATH/lcov" \
-        npm run test:unit
+if grep -q "test:coverage" package.json; then
+    echo "Running tests with coverage script..."
+    npm run test:coverage -- --coverage.enabled=true --coverage.reporter=lcov
 else
-    echo "Running tests with test script..."
-    
-    npx nyc \
-        --temp-directory="$COVERAGE_REPORT_PATH/nyc" \
-        --reporter=lcov \
-        --report-dir="$COVERAGE_REPORT_PATH/lcov" \
-        npm run test
+    echo "Running unit tests with coverage..."
+    npm run test:unit -- --coverage
 fi
+
+bash ../find-and-move-lcov.sh
 set -e
