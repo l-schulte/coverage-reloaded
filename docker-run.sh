d2a34643e87d0471ca6736c8f9bb01f124049b20
Lukas <lukas.schulte@uni-passau.de>
2026-02-10 16:29:57 +0100

    adjusts docker-run script to potentially support podman

--- docker-run.sh
+++ docker-run.sh
@@ -9,25 +9,28 @@
 
 echo "Script called as: $0 $@"
 
+# Change to "podman" if necessary
+EXECUTOR="docker"
+
 BASE_CONTAINER_NAME=core_node"$6"_base
 CONTAINER_NAME=core_node"$6"_"$1"
 CONTAINER_DIR=/coverage_reloaded
 
-docker build --build-arg NODE_VERSION=$6 -t $BASE_CONTAINER_NAME .
+$EXECUTOR build --build-arg NODE_VERSION=$6 -t $BASE_CONTAINER_NAME .
 
-docker build --build-arg NODE_VERSION=$6 -t $CONTAINER_NAME ./projects/$1 
-docker volume create core_"$1"_data
+$EXECUTOR build --build-arg NODE_VERSION=$6 -t $CONTAINER_NAME ./projects/$1 
+$EXECUTOR volume create core_"$1"_data
 
 mkdir -p projects/$1/output
 
 if [ "$2" = "shell" ]; then
     # Run an interactive container for testing, executes bash on start
-    docker run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash
+    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash
 elif [ "$2" = "debug" ]; then
     # Run an interactive container for debugging, executes bash and mounts the debug folder
     mkdir -p projects/$1/debug
-    docker run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v "$(pwd)/projects/$1/debug:$CONTAINER_DIR" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash
+    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v "$(pwd)/projects/$1/debug:$CONTAINER_DIR" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash
 elif [ "$2" = "exec" ]; then
     # Run the full process non-interactively
-    docker run --rm --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash execute.sh
+    $EXECUTOR run --rm --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" $CONTAINER_NAME bash execute.sh
 fi
