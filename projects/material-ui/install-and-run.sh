2da75979b4b8275c94ed41cad00aa54768bed862
Lukas <lukas.schulte@uni-passau.de>
2026-02-17 10:11:00 +0100

    adds material-ui project

---
+++ projects/material-ui/install-and-run.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+cd /coverage_reloaded/repo
+
+
+if $IS_NPM_MAIN_PM; then
+    echo "Installing dependencies with npm..."
+    npm install --no-fund --include=dev
+
+    COMMAND="npm run"
+elif $IS_YARN_MAIN_PM; then
+    echo "Installing dependencies with yarn..."
+
+    # Requires a non-existing dependency (vitest-mui, was deleted from registry)
+    # Workaround: providing the file (vitest-mui-0.3.3.tgz and vitest-mui-0.3.4.tgz) in the waypack machine
+    # Integrity checks fail for this file.
+    # Workaround: clean cache with yarn cache clean disable integrity checks for yarn installs by adding --update-checksums
+    echo "-> Installing dependencies via yarn..."
+    yarn cache clean --force
+    yarn install --update-checksums
+
+    COMMAND="yarn run"
+elif $IS_PNPM_MAIN_PM; then
+    echo "Installing dependencies with pnpm..."
+    pnpm install
+
+    COMMAND="pnpm run"
+else
+    echo "No main package manager detected... raising error."
+    exit 1
+fi
+
+set +e
+
+if grep -q '"test:coverage"' package.json; then
+    echo "Running tests with test:coverage script..."
+    $COMMAND test:coverage
+elif grep -q '"test:coverage:ci"' package.json; then
+    # test:coverage:ci is pre-configured to collect lcov reports.
+    echo "Running tests with test:coverage:ci script..."
+    $COMMAND test:coverage:ci
+else
+    echo "No test script found in package.json, skipping tests."
+fi  
+
+npx --registry=$WAYPACK_NPM_REGISTRY nyc report --reporter=lcov --report-dir="$COVERAGE_REPORT_PATH"
+    
+set -e
--- src/helpers/node/parse_version.py
+++ src/helpers/node/parse_version.py
@@ -8,13 +8,19 @@ NODE_RELEASES_PATH = "src/helpers/node/data/node_releases.json"
 NODE_RELEASES = json.load(open(NODE_RELEASES_PATH, "r"))
 
 
-def parse_node_version(version_string: str, use_first: bool = False) -> str | None:
+def parse_node_version(
+    version_string: str,
+    use_first: bool = False,
+    use_artificial_minor_version: bool = True,
+) -> str | None:
     """
     Parses a Node.js version string to extract the version.
 
     Parameters:
     - version_string: The version string to parse.
     - use_first: If True, returns the first matching version instead of the last.
+    - use_artificial_minor_version: If True, we upgrade each node release version to an artificially high minor version.
+                                    This is usefull because the containers download the latest version anyways.
     """
 
     # Check if range_str is single concrete version -> return as-is
@@ -24,8 +30,13 @@ def parse_node_version(version_string: str, use_first: bool = False) -> str | No
     last_ok = None
     for major in NODE_RELEASES.keys():
         major = major.removeprefix("v")
+
+        version = major
+        if use_artificial_minor_version:
+            version = f"{major}.9999"
+
         try:
-            if version_satisfies(major, version_string):
+            if version_satisfies(version, version_string):
                 last_ok = major
 
                 if use_first:
@@ -33,7 +44,7 @@ def parse_node_version(version_string: str, use_first: bool = False) -> str | No
 
         except Exception:
             raise ValueError(
-                f"npm satisfies check failed for version '{major}' and range '{version_string}'"
+                f"npm satisfies check failed for version '{version}' and range '{version_string}'"
             )
 
     return str(last_ok)
