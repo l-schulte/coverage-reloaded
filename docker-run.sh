5a687242517de7d60d000dcd698d97b4bcd0173e
Lukas Schulte <lukas.schulte@uni-passau.de>
2026-03-17 15:41:25 +0000

    removes unused volume

--- docker-run.sh
+++ docker-run.sh
@@ -19,18 +19,17 @@ CONTAINER_DIR=/coverage_reloaded
 $EXECUTOR build --build-arg NODE_VERSION=$6 -t $BASE_CONTAINER_NAME .
 
 $EXECUTOR build --build-arg NODE_VERSION=$6 -t $CONTAINER_NAME ./projects/$1 
-$EXECUTOR volume create core_"$1"_data 2>/dev/null || true
 
 mkdir -p projects/$1/output
 
 if [ "$2" = "shell" ]; then
     # Run an interactive container for testing, executes bash on start
-    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash
+    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash
 elif [ "$2" = "debug" ]; then
     # Run an interactive container for debugging, executes bash and mounts the debug folder
     mkdir -p projects/$1/debug
-    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v "$(pwd)/projects/$1/debug:$CONTAINER_DIR" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash
+    $EXECUTOR run --rm -it --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v "$(pwd)/projects/$1/debug:$CONTAINER_DIR" -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash
 elif [ "$2" = "exec" ]; then
     # Run the full process non-interactively
-    $EXECUTOR run --rm --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -v core_"$1"_data:$CONTAINER_DIR/data -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash execute.sh
+    $EXECUTOR run --rm --network mining-net --cap-add=NET_ADMIN --env-file .env -v "$(pwd)/projects/$1/output:$CONTAINER_DIR/coverage" -e "revision=$3" -e "timestamp=$4" -e "package_manager=$5" -e "project_id=$7" --pids-limit 10000 $CONTAINER_NAME bash execute.sh
 fi
