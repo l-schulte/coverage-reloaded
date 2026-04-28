c24a2b02fe1b56ea88b3a8328a16ce7fffb7c87d
Lukas Schulte <lukas.schulte@uni-passau.de>
2026-04-28 15:07:50 +0000

    adds option for prefixed lcov files

--- find-and-move-lcov.sh
+++ find-and-move-lcov.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Input value for which type of test was executed last, e.g. unit, integration, etc.
+TEST_TYPE="${1:-}"
+
+echo "Looking for locv files... $TEST_TYPE"
+
 cd /coverage_reloaded/repo
 
 # Define patterns to ignore
@@ -14,6 +19,13 @@ done
 # Create the target directory if it doesn't exist
 mkdir -p "$COVERAGE_REPORT_PATH"
 
+# Determine the destination filename
+if [ -n "$TEST_TYPE" ]; then
+    dest_filename="${TEST_TYPE}.lcov.info"
+else
+    dest_filename="lcov.info"
+fi
+
 # Find all lcov.info files anywhere in the repository
 while IFS= read -r -d '' lcov_file; do
     # Get the directory containing the lcov.info file
@@ -23,11 +35,11 @@ while IFS= read -r -d '' lcov_file; do
     rel_path="${rel_path#./}"
     dest_dir="$COVERAGE_REPORT_PATH/$rel_path"
     mkdir -p "$dest_dir"
-    cp "$lcov_file" "$dest_dir/"
+    mv "$lcov_file" "$dest_dir/$dest_filename"
 done < <(find . -type f -name "lcov.info" "${ignore_args[@]}" -print0)
 
-# Check if any lcov.info files exist in $COVERAGE_REPORT_PATH
-lcov_count=$(find "$COVERAGE_REPORT_PATH" -name "lcov.info" | wc -l)
+# Check if any prefixed lcov files exist in $COVERAGE_REPORT_PATH
+lcov_count=$(find "$COVERAGE_REPORT_PATH" -name "*.lcov.info" -o -name "lcov.info" | wc -l)
 if [ "$lcov_count" -eq 0 ]; then
     echo "Error: No lcov.info files found in any of the specified paths"
     exit 1
