6ba03ad2e1717c5dbf972ddc8b0975c5577021de
Lukas Schulte <lukas.schulte@uni-passau.de>
2026-05-12 08:28:11 +0000

    adds project pf2e

---
+++ projects/pf2e/install-and-run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+source "$(dirname "${BASH_SOURCE[0]}")/logging.sh"
+
+cd /coverage_reloaded/repo
+
+print_header 2 "Installing dependencies"
+
+npm install --no-fund --include=dev --legacy-peer-deps
+
+print_header 2 "Running tests"
+set +e
+
+npm test -- --coverage
+
+bash ../find-and-move-lcov.sh jest
+    
+set -e
